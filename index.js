3a1ba634f579bd15b501ea3a2e16c30ced215f67
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-25 13:43:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,11 @@ class BoardMember {
       return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
     };
   }
+
+  class Ceo extends BoardMember {
+    super.veto();
+    super.approve();
+    super.doCharity();
+    super.releasePressStatement();
+    super.sayHi();
+  }
