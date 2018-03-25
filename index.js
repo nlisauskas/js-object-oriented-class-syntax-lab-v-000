766c6fd7a0c6eeba831e94f2c3c35f7ab05b0641
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-25 13:40:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ class BoardMember {
     this.homeState = homeState;
     this.training = training;
 
-    veto = function (){
+    veto(){
       return "No, I must disagree"
     };
-    approve = function (){
+    approve(){
       return "You can do that!"
     };
-    doCharity = function (){
+    doCharity(){
       return "I like to help people."
     };
-    releasePressStatement = function (){
+    releasePressStatement(){
       return "You will see great things from Scuber."
     };
-    sayHi = function (){
+    sayHi(){
       return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
     };
   }
