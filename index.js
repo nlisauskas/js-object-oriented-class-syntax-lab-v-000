f5713aebc62c2c6e807a94bba33808a760f409bb
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-25 13:45:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+class BoardMember {
+  constructor(name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+}
+    veto(){
+      return "No, I must disagree"
+    };
+    approve(){
+      return "You can do that!"
+    };
+    doCharity(){
+      return "I like to help people."
+    };
+    releasePressStatement(){
+      return "You will see great things from Scuber."
+    };
+    sayHi(){
+      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+    };
+  }
+
+  class Ceo extends BoardMember {
+    veto() {super.veto();}
+
+    approve() {
+      super.approve();
+    }
+
+    doCharity() {super.doCharity();}
+
+    releasePressStatement() {
+    super.releasePressStatement();
+    }
+
+    sayHi() {
+    super.sayHi();
+    }
+
+    hireEmployee() {
+      return "Welcome aboard!"
+    }
+
+  }
